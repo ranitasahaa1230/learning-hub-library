05f16779fb561c40c036d89f05883b8efd727930
Ranita Saha <saharanitaa1230@gmail.com>
2022-04-15 12:08:08 +0530

    feat: light theme added

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import { Footer, Header, NotFound } from "./components";
 import { Route, Routes } from "react-router-dom";
 import { History, Home, LikedVideo, Playlist, WatchLater } from "./pages";
 import Mockman from "mockman-js";
+import { useTheme } from "./contexts";
 
 function App() {
   const [sidebars, toggleSidebar] = useState(false);
+  const {theme}=useTheme();
 
   const handleToggleSidebar = () => toggleSidebar((prev) => !prev);
 
   return (
-    <div className="App">
+    <div className={theme ? "light__mode" : "dark__mode"}>
       <Header handleToggleSidebar={handleToggleSidebar} />
       <Routes>
         <Route
