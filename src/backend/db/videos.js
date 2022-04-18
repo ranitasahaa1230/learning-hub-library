c3c3cc94436d77dce11c46fdaf7e0ef7089272bb
Ranita Saha <saharanitaa1230@gmail.com>
2022-04-19 01:54:47 +0530

    update db

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -5,17 +5,279 @@
 
 export const videos = [
   {
-    _id: "Wo5dMEP_BbI",
-    title: "Awesome Video about Coding",
+    _id: "Ke90Tje7VS0",
+    title: "React Tutorial for Beginners",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    creator: "Soham Shah",
+      "React JS Tutorial - Get up & running with React JS: the most popular JavaScript library in the world!",
+    creator: "Programming with Mosh",
+    category: "React JS",
+    uploaded: "16 July 2021",
+    comments: [
+      {
+        user: "Sikanta Trailor",
+        comment:
+          "I am going to put 'Trained by Mosh' on my resume and get all the jobs!💪🏻",
+        color: "hotpink",
+      },
+      {
+        user: "Roma Chaudhury",
+        comment:
+          "Loved this video. This guy explains react better than the documentations. we are blessed to be living in the age of internet.",
+        color: "brown",
+      },
+    ],
   },
   {
-    _id: "F_Riqjdh2oM",
-    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
-    creator: "Sentdex",
+    _id: "Fnlnw8uY6jo",
+    title: "Namaste JavaScript Course",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+      "Hoisting in JavaScript is the most famous Interview Question. And it is also one of the most misunderstood concepts in JS. This video will cover in-depth knowledge and fundamentals behind variable and function hoisting.",
+    creator: "Akshay Saini",
+    category: "Javascript",
+    uploaded: "18 October 2018",
+    comments: [
+      {
+        user: "Suhanya Singh",
+        comment:
+          "This playlist is absolutely amazing 🔥 and extremely recommended for developers",
+        color: "hotpink",
+      },
+      {
+        user: "Nidhi Vasa",
+        comment:
+          "The person has a positive approach towards life. A perfect team player.",
+        color: "brown",
+      },
+    ],
+  },
+  {
+    _id: "1oU_YGhT7ck",
+    title: "Complete Redux Tutorial",
+    description:
+      "Welcome, to Complete Redux Tutorial for beginners in 2021. We will cover React with Redux and create an project with it",
+    creator: "Thapa Technical",
+    category: "Redux",
+    uploaded: "3 May 2021",
+    comments: [
+      {
+        user: "Bablu Singh",
+        comment:
+          "I was struggling with redux so much, but you cleared the whole concept for me. Thank You.",
+        color: "hotpink",
+      },
+      {
+        user: "Roma Tripathi",
+        comment:
+          "Redux is just about understanding the flow of data and you made it so easy. Great work.",
+        color: "brown",
+      },
+    ],
+  },
+  {
+    _id: "oSIv-E60NiU",
+    title: "MongoDB Tutorial",
+    description:
+      "In this video, we will see the MongoDB NoSQL database in Hindi very closely. I have included a cheat sheet with this course which makes it easy for you to understand MongoDB",
+    creator: "CodeWithHarry",
+    category: "MongoDB",
+    uploaded: "Dec 12 2021",
+    comments: [
+      {
+        user: "Ritvik Singh",
+        comment:
+          "This MongoDB course is definitely helpful.",
+        color: "hotpink",
+      },
+      {
+        user: "Roma Vasa",
+        comment:
+          "Amazing explanation, anyone with basic knowledge of any DBMS can relate to it!",
+        color: "brown",
+      },
+    ],
+  },
+  {
+    _id: "gp5H0Vw39yw",
+    title: "Learn TypeScript - Full Course for Beginners",
+    description:
+      "Learn the TypeScript programming language in this crash course for beginners. By the end of this course you will understand all modern TypeScript features.",
+    creator: "freeCodeCamp",
+    category: "TypeScript",
+    uploaded: "15 March 2018",
+    comments: [
+      {
+        user: "Rituja Trailor",
+        comment:
+          "Excellent intro to TypeScript for beginners and this also serve as refresher for experienced devs.",
+        color: "hotpink",
+      },
+      {
+        user: "Roma Vasa",
+        comment:
+          "That was incredibly cool! Nice course i've learned so much about ts. Now I'm ready to start testing all this stuff.",
+        color: "brown",
+      },
+    ],
+  },
+  {
+    _id: "b9eMGE7QtTk",
+    title: "React JS Crash Course",
+    description:
+      "Are you wondering how to become a React developer? Look no further, and get all the info in one video.",
+    creator: "JavaScript Mastery",
+    category: "React JS",
+    uploaded: "4 March 2022",
+    comments: [
+      {
+        user: "Rahul Singhania",
+        comment:
+          "This is absolutely my first react project and the first time I got to understand the concepts. You are a great teacher.",
+        color: "hotpink",
+      },
+      {
+        user: "Ramyani Bulani",
+        comment:
+          "My Confidence level up after watching every video your channel",
+        color: "brown",
+      },
+    ],
+  },
+  {
+    _id: "hEX8ohGSOyc",
+    title: "User Role in Javascript",
+    description:
+      "Welcome to javascript course. This is a new javascript course designed, created and recorded fresh in 2019. This course will give you a fantastic start for your javascript journey. ",
+    creator: "Hitesh Choudhary",
+    category: "Javascript",
+    uploaded: "22 March 2019",
+    comments: [
+      {
+        user: "Bablu Singh",
+        comment:
+          "You are such a good teacher, thank you!",
+        color: "hotpink",
+      },
+      {
+        user: "Roma Vaini",
+        comment:
+          "Awesome series, I am loving it day by day. Best teacher in the world.",
+        color: "brown",
+      },
+    ],
+  },
+  {
+    _id: "93p3LxR9xfM",
+    title: "Redux Crash Course With React",
+    description:
+      "In this video we will talk about what Redux is and build a React app from scratch and add all of the boilerplate for Redux and work with the store/state, actions, reducers and all of the other fundamentals of the Redux state manager.",
+    creator: "Traversy Media",
+    category: "Redux",
+    uploaded: "17 March 2018",
+    comments: [
+      {
+        user: "Rohan Singhania",
+        comment:
+          "Brad is doing the lord's work.",
+        color: "hotpink",
+      },
+      {
+        user: "Divya Tripathi",
+        comment:
+          "Great tutorial, thanks!",
+        color: "brown",
+      },
+    ],
+  },
+  {
+    _id: "WBPrJSw7yQA",
+    title: "Learn TypeScript",
+    description:
+      "TypeScript is an open-source programming language developed and maintained by Microsoft. It is a strict syntactical superset of JavaScript, and adds optional static typing to the language.",
+    creator: "Codevolution",
+    category: "TypeScript",
+    uploaded: "6 Aug 2018",
+    comments: [
+      {
+        user: "Bablu Amarnath",
+        comment:
+          "This was extremely valuable! Thank you for this condensed tutorial. Lots of great information.",
+        color: "hotpink",
+      },
+      {
+        user: "Roma Poddar",
+        comment:
+          "You've made Typescript so much easier than other tutorials 🔥 Definitely helpful !",
+        color: "brown",
+      },
+    ],
+  },
+  {
+    _id: "pWbMrx5rVBE",
+    title: "MongoDB In 30 Minutes",
+    description:
+      "In this video we will dive into the MongoDB NoSQL database and look at the fundamentals and the syntax to create, read, update and delete documents/data",
+    creator: "Traversy Media",
+    category: "MongoDB",
+    uploaded: "5 June 2016",
+    comments: [
+      {
+        user: "Prithvi Raj",
+        comment:
+          "Love your tutorials..Simple and to the point. Thanks a lot!",
+        color: "hotpink",
+      },
+      {
+        user: "Akasa Singh",
+        comment:
+          "Hey I just wanted to say thank you. Great video",
+        color: "brown",
+      },
+    ],
+  },
+  {
+    _id: "8pZ3ZDWEiRA",
+    title: "React JS Advanced Tutorial",
+    description:
+      "In this react js Hindi tutorial we learn advanced topics. we get to know what is Recoil library in react js with a simple example.",
+    creator: "Code Step By Step",
+    category: "React JS",
+    uploaded: "23 June 2020",
+    comments: [
+      {
+        user: "Bablu Singh",
+        comment:
+          "Nice video, Sir.I truly appreciate this video.",
+        color: "hotpink",
+      },
+      {
+        user: "Ronita Sharma",
+        comment:
+          "Loved this video.",
+        color: "brown",
+      },
+    ],
+  },
+  {
+    _id: "dWZIPIc3szg",
+    title: "this in JavaScript",
+    description:
+      "Explained this keyword and How the value of this changes dynamically with practical examples.",
+    creator: "CodeWellTech",
+    category: "JavaScript",
+    uploaded: "14 Dec 2021",
+    comments: [
+      {
+        user: "Tushar Samanta",
+        comment:
+          "Thank you so much for this video!",
+        color: "hotpink",
+      },
+      {
+        user: "Anand Bulani",
+        comment:
+          "Loved this video. Very good content!",
+        color: "brown",
+      },
+    ],
   },
 ];
