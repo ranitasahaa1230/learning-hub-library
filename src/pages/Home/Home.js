9ec35af11ef6aded3a9a14a0f205268a1940d073
Ranita Saha <saharanitaa1230@gmail.com>
2022-04-20 16:14:29 +0530

    Update Home.js

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -6,11 +6,11 @@ import "./Home.css";
 export const Home = () => {
   return (
     // <div className="section__page">
-      <main className="app__container">
-        <Container fluid className="app__main">
-          <VideoListing />
-        </Container>
-      </main>
+    <main className="app__container">
+      <Container fluid className="app__main">
+        <VideoListing />
+      </Container>
+    </main>
     // </div>
   );
 };
