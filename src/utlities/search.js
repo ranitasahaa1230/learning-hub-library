0b398225f26f5b12921c26d2c793b7b5e773c8d3
Ranita Saha <saharanitaa1230@gmail.com>
2022-04-22 16:18:17 +0530

    update query

--- src/utlities/search.js
+++ src/utlities/search.js
@@ -1,6 +1,6 @@
 const getSearchedVideos = (videoListing, searchQuery) => {
     return videoListing.filter((video) =>
-      video.title.toLowerCase().includes(searchQuery)
+      video.title.toLowerCase().includes(searchQuery.toLowerCase())
     );
   };
   export { getSearchedVideos };
