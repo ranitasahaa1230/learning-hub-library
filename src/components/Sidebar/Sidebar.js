e9b2f0ddb65bdb5887f89f1f02168795e2059a92
Ranita Saha <saharanitaa1230@gmail.com>
2022-04-27 16:32:49 +0530

    Update Sidebar.js

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -35,55 +35,55 @@ export const Sidebar = () => {
       className={`${sidebars ? "sidebar open" : "sidebar"}`}
       onClick={() => handleToggleSidebar(false)}
     >
-      <NavLink to="/" className="navlink">
-        <li>
-          <MdHome size={25} />
-          <span>Home</span>
-        </li>
-      </NavLink>
-
-      <NavLink to="/explore" className="navlink">
-        <li>
-          <MdOutlineExplore size={25} />
-          <span>Explore</span>
-        </li>
-      </NavLink>
-      <NavLink to="/playlists" className="navlink">
-        <li>
-          <MdPlaylistAdd size={25} />
-          <span>Playlists</span>
-        </li>
-      </NavLink>
-      <NavLink to="/watch-later" className="navlink">
-        <li>
-          <MdOutlineWatchLater size={25} />
-          <span>Watch Later </span>
-        </li>
-      </NavLink>
-      <NavLink to="/liked-videos" className="navlink">
-        <li>
-          <MdThumbUp size={25} />
-          <span>Liked Videos </span>
-        </li>
-      </NavLink>
-      <li>
-        <NavLink to="/history" className="navlink">
-          <MdHistory size={25} />
-          <span>History</span>
-        </NavLink>
-      </li>
-      <hr />
-
-      {isAuth && (
-        <>
-          <li onClick={logoutHandler}>
-            <MdExitToApp size={25} />
-            <span>Log Out</span>
+        <NavLink to="/" className="navlink">
+          <li>
+            <MdHome size={25} />
+            <span>Home</span>
           </li>
+        </NavLink>
 
-          <hr />
-        </>
-      )}
+        <NavLink to="/explore" className="navlink">
+          <li>
+            <MdOutlineExplore size={25} />
+            <span>Explore</span>
+          </li>
+        </NavLink>
+        <NavLink to="/playlists" className="navlink">
+          <li>
+            <MdPlaylistAdd size={25} />
+            <span>Playlists</span>
+          </li>
+        </NavLink>
+        <NavLink to="/watch-later" className="navlink">
+          <li>
+            <MdOutlineWatchLater size={25} />
+            <span>Watch Later </span>
+          </li>
+        </NavLink>
+        <NavLink to="/liked-videos" className="navlink">
+          <li>
+            <MdThumbUp size={25} />
+            <span>Liked Videos </span>
+          </li>
+        </NavLink>
+        <NavLink to="/history" className="navlink">
+          <li>
+            <MdHistory size={25} />
+            <span>History</span>
+          </li>
+        </NavLink>
+        <hr />
+
+        {isAuth && (
+          <>
+            <li onClick={logoutHandler}>
+              <MdExitToApp size={25} />
+              <span>Log Out</span>
+            </li>
+
+            <hr />
+          </>
+        )}
     </aside>
   );
 };
